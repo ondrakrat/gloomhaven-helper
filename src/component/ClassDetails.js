862c902485215f22ec3f823f6a00d2888e9e3d28
Ondřej Kratochvíl <ondrakratochvil@seznam.cz>
2020-10-10 19:53:38 +0200

    Implement build tag statistics

--- src/component/ClassDetails.js
+++ src/component/ClassDetails.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import { union, frequencyMap } from '../constants';
+import { Tag } from '../game/Skill';
 
 const useStyles = makeStyles({
     slider: {
         maxWidth: '250px'
+    },
+    hidden: {
+        display: 'none'
+    },
+    table: {
+        maxWidth: 'vmax'
     }
 });
 
@@ -13,11 +28,18 @@ const sliderLabels = [...Array(9).keys()]
     .map(i => i + 1)
     .map(i => ({'value': i, 'label': i}))
 
+const collectTags = (build, skills) => build
+    .map(skill => skills[skill])
+    .flatMap(skill => union(skill.top.tags, skill.bottom.tags))
+    .reduce(frequencyMap, {});
+
 function ClassDetails(props) {
     const classes = useStyles();
     const clazz = props.clazz;
     const build = props.build;
     const skills = props.skills;
+    const collectedTags = collectTags(build, skills);
+    const sortedKeys = Object.keys(collectedTags).sort((a, b) => Object.values(Tag).indexOf(a) - Object.values(Tag).indexOf(b));
     return (
         <div>
             <Typography variant="h2" gutterBottom>
@@ -51,6 +73,25 @@ function ClassDetails(props) {
             <Typography paragraph>
                 {`Average initiative: ${build.length === 0 ? 'N/A' : Math.round(build.map(skill => skills[skill].initiative).reduce((s1, s2) => s1 + s2, 0) / build.length)}`}
             </Typography>
+            <TableContainer component={Paper} className={build.length === 0 ? classes.hidden : 'mobile-hide'}>
+                <Table className={classes.table} aria-label="caption table">
+                    <caption>Number of cards with given tag (equal tags on both top and bottom half count as a single card)</caption>
+                    <TableHead>
+                        <TableRow>
+                            {sortedKeys.map(tag => (
+                                <TableCell key={tag}>{tag}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            {sortedKeys.map((tag, i) => (
+                                <TableCell key={`freq-${i}`}>{collectedTags[tag]}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 }
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,13 @@
 export const DraggableTypes = {
     SkillCard: 'SkillCard'
 }
+
+export const union = (array1, array2) => {
+    const result = [...array1, ...array2];
+    return result.filter((item, pos) => result.indexOf(item) === pos);
+}
+
+export const frequencyMap = (a1, a2) => {
+    a1[a2] = a1.hasOwnProperty(a2) ? ++a1[a2] : 1;
+    return a1;
+}
