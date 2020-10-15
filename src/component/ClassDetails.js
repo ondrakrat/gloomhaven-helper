a74c22b41cbb6c22db969dc6c69d15a561dc92f6
Ondřej Kratochvíl <ondrakratochvil@seznam.cz>
2020-10-15 18:34:06 +0200

    Move statistics to modal

--- src/component/ClassDetails.js
+++ src/component/ClassDetails.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
+import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,11 +9,14 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import { makeStyles } from '@material-ui/core/styles';
 import { union, frequencyMap } from '../constants';
 import { Tag } from '../game/Skill';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     slider: {
         maxWidth: '250px'
     },
@@ -21,8 +25,19 @@ const useStyles = makeStyles({
     },
     table: {
         maxWidth: 'vmax'
-    }
-});
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const sliderLabels = [...Array(9).keys()]
     .map(i => i + 1)
@@ -40,6 +55,8 @@ function ClassDetails(props) {
     const skills = props.skills;
     const collectedTags = collectTags(build, skills);
     const sortedKeys = Object.keys(collectedTags).sort((a, b) => Object.values(Tag).indexOf(a) - Object.values(Tag).indexOf(b));
+
+    const [modalOpen, setModalOpen] = useState(false);
     return (
         <div>
             <Typography variant="h2" gutterBottom>
@@ -61,37 +78,57 @@ function ClassDetails(props) {
                 marks={sliderLabels}
                 onChange={props.levelChanged}
             />
-            <Typography variant="h4" gutterBottom>
-                Build statistics:
-            </Typography>
-            <Typography paragraph>
-                {`Lowest initiative: ${build.length === 0 ? 'N/A' : Math.min(...build.map(skill => skills[skill].initiative))}`}
-            </Typography>
-            <Typography paragraph>
-                {`Highest initiative: ${build.length === 0 ? 'N/A' : Math.max(...build.map(skill => skills[skill].initiative))}`}
-            </Typography>
-            <Typography paragraph>
-                {`Average initiative: ${build.length === 0 ? 'N/A' : Math.round(build.map(skill => skills[skill].initiative).reduce((s1, s2) => s1 + s2, 0) / build.length)}`}
-            </Typography>
-            <TableContainer component={Paper} className={build.length === 0 ? classes.hidden : 'mobile-hide'}>
-                <Table className={classes.table} aria-label="caption table">
-                    <caption>Number of cards with given tag (equal tags on both top and bottom half count as a single card)</caption>
-                    <TableHead>
-                        <TableRow>
-                            {sortedKeys.map(tag => (
-                                <TableCell key={tag}>{tag}</TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            {sortedKeys.map((tag, i) => (
-                                <TableCell key={`freq-${i}`}>{collectedTags[tag]}</TableCell>
-                            ))}
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <div>
+                <Button variant="contained" color="primary" onClick={() => setModalOpen(true)}>Build statistics</Button>
+            </div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={modalOpen}
+                onClose={() => setModalOpen(false)}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 500,
+                }}
+            >
+                <Fade in={modalOpen}>
+                    <div className={classes.paper}>
+                        <Typography variant="h4" gutterBottom>
+                            Build statistics:
+                        </Typography>
+                        <Typography paragraph>
+                            {`Lowest initiative: ${build.length === 0 ? 'N/A' : Math.min(...build.map(skill => skills[skill].initiative))}`}
+                        </Typography>
+                        <Typography paragraph>
+                            {`Highest initiative: ${build.length === 0 ? 'N/A' : Math.max(...build.map(skill => skills[skill].initiative))}`}
+                        </Typography>
+                        <Typography paragraph>
+                            {`Average initiative: ${build.length === 0 ? 'N/A' : Math.round(build.map(skill => skills[skill].initiative).reduce((s1, s2) => s1 + s2, 0) / build.length)}`}
+                        </Typography>
+                        <TableContainer component={Paper} className={build.length === 0 ? classes.hidden : 'mobile-hide'}>
+                            <Table className={classes.table} aria-label="caption table">
+                                <caption>Number of cards with given tag (equal tags on both top and bottom half count as a single card)</caption>
+                                <TableHead>
+                                    <TableRow>
+                                        {sortedKeys.map(tag => (
+                                            <TableCell key={tag}>{tag}</TableCell>
+                                        ))}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        {sortedKeys.map((tag, i) => (
+                                            <TableCell key={`freq-${i}`}>{collectedTags[tag]}</TableCell>
+                                        ))}
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                </Fade>
+            </Modal>
         </div>
     );
 }
